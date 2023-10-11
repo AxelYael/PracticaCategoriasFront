5f331a314d656cb2721690d7f808ec4754420f17
AxelYael <axelyael@ciencias.unam.mx>
2023-10-11 03:24:37 -0600

    Practica07 terminada

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
-export class ProductModule { }
+export class ProductModule { 
+  constructor() {
+    console.log('ProductModule loaded.');
+  }
+}

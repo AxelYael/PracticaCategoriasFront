88de375e01ffd0d0cb9f80aad45615041682ad68
AxelYael <axelyael@ciencias.unam.mx>
2023-10-23 00:49:16 -0600

    Se agrega practicao8 terminada

--- src/app/modules/components/category.component.ts
+++ src/app/modules/components/category.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { Category } from 'src/app/modules/_models/category';
 import { FormBuilder, Validators } from '@angular/forms';
+
 
 declare var $: any;
 
@@ -12,58 +12,113 @@ declare var $: any;
 })
 
 export class CategoryComponent implements OnInit {
-  categories: Category[] = [];
-  categoryForm!: FormGroup; 
- 
+categories: Category[] = [];
+  categoryUpdated: number = 0;
+  categoryForm = this.formBuilder.group({
+    category: ["", [Validators.required]],
+    code: ["", [Validators.required]],
+  });
+  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }  
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getCategories();
-    this.initializeForm();
   }
 
-  private initializeForm(): void {
-    this.categoryForm = this.formBuilder.group({
-      category: ['', Validators.required], 
-      code: ['', Validators.required]
-    });
-  }
 
   getCategories(): void {
-    this.categories = [
-      new Category(1, 'AGJKL', 'Libros', 'activo'),
-      new Category(2, 'JKLJ', 'Limpieza', 'activo'),
-      new Category(3, 'LPJOP', 'Electronicos', 'inactivo'),
+    let category1 = new Category(1, 'ELEC', 'Electrónicos', 1);
+    let category2 = new Category(2, 'LIB', 'Libros', 1);
+    let category3 = new Category(3, 'ROP', 'ROPA', 0);
 
-      
-    ];
+    this.categories.push(category1, category2, category3);
   }
 
-  openModal(): void {
-    $('#myModal').modal('show');
-  }
 
-  submitted = false;
-  onSubmit(){
+  // Manejar el envío del formulario
+  onSubmit(): void {
     this.submitted = true;
-    if(this.categoryForm.invalid) return;
-    this.submitted = false;
-    // Obtener el último ID y sumarle 1 para el nuevo ID
-    let lastID = this.categories.length > 0 ? this.categories[this.categories.length - 1].category_id : 0;
-    let newID = lastID + 1;
+    if (this.categoryForm.invalid) {
+      return;
+    }
 
-    let newCategory = new Category(newID, this.categoryForm.value.code, this.categoryForm.value.category, 'activo');
-    this.categories.push(newCategory);
-    $("#modalForm").modal("hide");
-    alert("Región guardada exitosamente!");
-}
+    if (this.categoryUpdated == 0) {
+      this.onSubmitCreate();
+    } else {
+      this.onSubmitUpdate();
+    }
+  }
 
-showModalForm(){
+    // Crear una nueva categoría
+    onSubmitCreate(): void {
+      let newCategory = new Category(
+
+        this.categories.length > 0 ? this.categories[this.categories.length - 1].category_id + 1 : 1,
+        this.categoryForm.controls['category'].value!, 
+        this.categoryForm.controls['code'].value!, 1
+      );
+  
+      this.categories.push(newCategory);
+      alert('Categoría creada exitosamente!');
+    }
+  
+    // Actualizar una categoría existente
+    onSubmitUpdate(): void {
+      for (let category of this.categories) {
+        if (category.category_id == this.categoryUpdated) {
+          category.category = this.categoryForm.controls['category'].value!; 
+          category.code = this.categoryForm.controls['code'].value!; 
+          break;
+        }
+      }
+      alert('Categoría actualizada exitosamente!');
+      this.categoryUpdated = 0; 
+
+    }
+
+  updateCategory(category: Category){
+    this.categoryUpdated = category.category_id;
+    
     this.categoryForm.reset();
+    this.categoryForm.controls['category'].setValue(category.category);
+    this.categoryForm.controls['code'].setValue(category.code);
+    
     this.submitted = false;
     $("#modalForm").modal("show");
   }
+  
+  // modals 
+  showModalForm(){
+    this.categoryForm.reset();
+    this.categoryUpdated = 0;
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
+
+
+   // CRUD region
+   disableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 0;
+        alert("categoria desactivada exitosamente!");
+        break;
+      }
+    }
+    console.log("SALIR")
+  }
+  enableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 1;
+        alert("categoria activada exitosamente!");
+        break;
+      }
+    }
+  }
+
+  
 }
 
 
